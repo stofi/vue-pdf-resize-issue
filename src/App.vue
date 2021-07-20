454503b105c24fc608656f0745f95eb2c847182e
Krystof Rainisch <Krystof.Rainisch@hey.com>
2021-07-20 19:49:52 +0200

    add issue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <h1>Welcome to Your Vue.js App</h1>
+    <pdf
+      src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf"
+    ></pdf>
   </div>
 </template>
+
+<script>
+import Pdf from 'vue-pdf'
+
+export default {
+  components: {
+    Pdf,
+  },
+}
+</script>
